d952d463c6c77ea64e4e216daab134c253a2face
ᴮᵉᵗᵗᵉʳ ᴼᶠᶠ ᴳᵒⁿᵉ <56366540+BetterOffGone@users.noreply.github.com>
2019-11-07 21:26:21 +0000

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,80 @@
+class TextScramble {
+  constructor(el) {
+    this.el = el;
+    this.chars = '!@#$%^&*()_-=+{}:"|<>?,./;';
+    this.update = this.update.bind(this);
+  }
+  setText(newText) {
+    const oldText = this.el.innerText;
+    const length = Math.max(oldText.length, newText.length);
+    const promise = new Promise(resolve => (this.resolve = resolve));
+    this.queue = [];
+    for (let i = 0; i < length; i++) {
+      const from = oldText[i] || "";
+      const to = newText[i] || "";
+      const start = Math.floor(Math.random() * 40);
+      const end = start + Math.floor(Math.random() * 40);
+      this.queue.push({ from, to, start, end });
+    }
+    cancelAnimationFrame(this.frameRequest);
+    this.frame = 0;
+    this.update();
+    return promise;
+  }
+  update() {
+    let output = "";
+    let complete = 0;
+    for (let i = 0, n = this.queue.length; i < n; i++) {
+      let { from, to, start, end, char } = this.queue[i];
+      if (this.frame >= end) {
+        complete++;
+        output += to;
+      } else if (this.frame >= start) {
+        if (!char || Math.random() < 0.28) {
+          char = this.randomChar();
+          this.queue[i].char = char;
+        }
+        output += `<span class="dud">${char}</span>`;
+      } else {
+        output += from;
+      }
+    }
+    this.el.innerHTML = output;
+    if (complete === this.queue.length) {
+      this.resolve();
+    } else {
+      this.frameRequest = requestAnimationFrame(this.update);
+      this.frame++;
+    }
+  }
+  randomChar() {
+    return this.chars[Math.floor(Math.random() * this.chars.length)];
+  }
+}
+
+const phrases = [
+  "Hello Lurker, I can see you",
+  "Yes you, Welcome to my little playground",
+  "Here you can visit my Github and other projects I have developed over the years.",
+  "Little Tip For You: It's better to have knowledge",
+  "than fooling everyone including yourselves & not have knowledge",
+  "I am a Founder of InsanitySecurityTeam (now retired as of 2019)",
+  "I have spent numerous years studying Cyber Security",
+  "I now share my years of knowledge with everyone."
+];
+
+const el = document.querySelector(".text");
+const fx = new TextScramble(el);
+
+let counter = 0;
+const next = () => {
+  fx.setText(phrases[counter]).then(() => {
+    setTimeout(next, 2500);
+  });
+  counter = (counter + 1) % phrases.length;
+};
+
+next();
+
+
+document.addEventListener('DOMContentLoaded');
